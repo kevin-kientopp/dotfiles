4a1ae2170d33b0b644e6539cb0aea104208e5f3f
kevin-kientopp <kevinkientopp@gmail.com>
2017-01-06 00:38:43 -0700

    Add vim temp files to gitignore. Script now sets gitignore global.

--- create-symbolic-links.sh
+++ create-symbolic-links.sh
@@ -7,6 +7,7 @@ echo "creating symbolic link for .tmux.conf"
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
 echo "creating symbolic link for .gitignore_global"
-ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/gitignore ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
 
-ls -al ~/.tmux.conf ~/.vimrc ~/.gitignore_global
+ls -al ~/.tmux.conf ~/.vimrc ~/.gitignore
